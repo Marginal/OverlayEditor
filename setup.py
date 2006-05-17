2c3ee0cad867e5df08ee30a34f8faf7e9981af32
Jonathan Harris <x-plane@marginal.org.uk>
2006-05-17 03:03:32 +0000

    Import 100

---
+++ setup.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+from files import appversion
+from distutils.core import setup
+from os import listdir, name
+from sys import platform
+
+
+# bogus crud to get WinXP "Visual Styles"
+manifest=('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n'+
+          '<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">\n'+
+          '<assemblyIdentity\n'+
+          '    version="%s.0.0"\n' % appversion +
+          '    processorArchitecture="X86"\n'+
+          '    name="OverlayEditor"\n'+
+          '    type="win32"\n'+
+          '/>\n'+
+          '<description>DSF overlay editor.</description>\n'+
+          '<dependency>\n'+
+          '    <dependentAssembly>\n'+
+          '        <assemblyIdentity\n'+
+          '            type="win32"\n'+
+          '            name="Microsoft.Windows.Common-Controls"\n'+
+          '            version="6.0.0.0"\n'+
+          '            processorArchitecture="X86"\n'+
+          '            publicKeyToken="6595b64144ccf1df"\n'+
+          '            language="*"\n'+
+          '        />\n'+
+          '    </dependentAssembly>\n'+
+          '</dependency>\n'+
+          '</assembly>\n')
+
+
+if platform=='win32':
+    # http://www.py2exe.org/  Invoke with: setup.py py2exe
+    import py2exe
+    platdata=[('win32',
+               ['win32/DSFTool.exe',
+                'win32/OverlayEditor.ico',
+                ]),
+              ]
+
+elif platform.lower().startswith('darwin'):
+    # http://undefined.org/python/py2app.html  Invoke with: setup.py py2app
+    import py2app
+    platdata=[('MacOS',
+               ['MacOS/DSFTool',
+                #'MacOS/OverlayEditor.icns',
+                ]),
+              # Include wxPython 2.4
+              #('../Frameworks',
+              # ['/usr/local/lib/libwx_mac-2.4.0.rsrc',
+              #  ]),
+              ]
+
+setup(name='OverlayEditor',
+      version=appversion,
+      description='DSF overlay editor',
+      author='Jonathan Harris',
+      author_email='x-plane@marginal.org.uk',
+      url='http://marginal.org.uk/xplanescenery',
+      data_files=[('',
+                   ['OverlayEditor.html',
+                    ]),
+                  ('Resources',
+                   ['Resources/add.png',
+                    'Resources/delete.png',
+                    'Resources/goto.png',
+                    'Resources/import.png',
+                    'Resources/new.png',
+                    'Resources/open.png',
+                    'Resources/OverlayEditor.png',
+                    'Resources/prefs.png',
+                    'Resources/reload.png',
+                    'Resources/save.png',
+                    'Resources/screenshot.png',
+                    ]),
+                  ] + platdata,
+
+      options = {'py2exe': {'dll_excludes':['w9xpopen.exe'],
+                            'bundle_files':True,
+                            'compressed':True,
+                            'excludes':['socket', 'urllib', 'webbrowser'],
+                            'optimize':2,
+                            },
+                 'py2app': {'argv_emulation':False,
+                            'iconfile':'MacOS/OverlayEditor.icns',
+                            'includes':['wx'],
+                            'packages':['wx'],
+                            'frameworks':['wx'],
+                            'compressed':True,
+                            'optimize':2,
+                            'semi_standalone':True,
+                            },
+                 },
+
+      zipfile = None,
+      
+      # win32
+      windows = [{'script':'OverlayEditor.py',
+                  'icon_resources':[(1,'win32/OverlayEditor.ico')],
+                  'other_resources':[(24,1,manifest)],
+                  }],
+
+      # mac
+      #app = ['OverlayEditor.py'],
+)
